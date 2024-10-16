479e3492c7980b9c48845eabf9f7e2a409440d93
Vadim Narkevich <vadzimnarkevich@gmail.com>
2024-10-16 18:09:37 +0300

    Add css + move yandex button to fixed element

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -4,16 +4,16 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const config: webpack.Configuration = {
   entry: {
-    yandex: './src/yandex.ts',
+    yandex: './src/yandex/yandex.ts',
     youtube: './src/youtube.ts',
     background: './src/background.ts',
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'sharink-[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
   },
   module: {
     rules: [
@@ -22,13 +22,25 @@ const config: webpack.Configuration = {
         test: /\.ts$/,
         exclude: /node_modules/,
       },
+      {
+        use: ['css-loader'],
+        test: /\.css$/,
+        exclude: /node_modules/,
+      },
     ],
   },
   mode: 'development',
   watch: true,
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [{ from: 'static' }],
+      patterns: [
+        { from: 'static' },
+        {
+          from: '**/*.css', // adjust the path as needed
+          to: path.resolve(__dirname, 'dist/assets/sharink-[name][ext]'),
+          context: 'src', // only copy from the 'src' directory
+        },
+      ],
     }),
   ],
 };
